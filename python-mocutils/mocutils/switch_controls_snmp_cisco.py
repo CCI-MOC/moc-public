273e99815e752f97ed7f29f7367a5bf8b6b8b922
LinZhaoliang <liuzhaoliang1988@gmail.com>
2013-08-23 14:34:16 -0400

    Create switch_controls_snmp_cisco.py

---
+++ python-mocutils/mocutils/switch_controls_snmp_cisco.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python
+import os
+def get_dot10said(vlan_id):
+	#expect vlan_id as a str
+	dot10said=100000+int(vlan_id)
+	#print dot10said
+	dot10said="%x"%(dot10said)
+	return dot10said.zfill(8)
+def make_remove_vlans(vlan_ids,add,switch_ip='192.168.0.254'):
+	# Expects that you send a string which is a comma separated list of vlan_ids and a bool for adding or removing
+	for vlan_id in vlan_ids.split(','):
+		if add:
+			dot10said=get_dot10said(vlan_id)
+			print dot10said
+			os.system('snmpset -v2c -c private '+switch_ip+' vtpVlanEditOperation.1 i 2')
+			os.system('snmpset -v2c -c private '+switch_ip+' vtpVlanEditRowStatus.1.'+vlan_id+' i 4')
+			os.system('snmpset -v2c -c private '+switch_ip+' vtpVlanEditType.1.'+vlan_id+' i 1')
+			os.system('snmpset -v2c -c private '+switch_ip+' vtpVlanEditName.1.'+vlan_id+' s "vlan_'+vlan_id+'"')
+
+			#Below is to set a strange(?) so-called dot10said, which is a hex of 100000+vlan_id
+			os.system('snmpset -v2c -c private '+switch_ip+' vtpVlanEditDot10Said.1.'+vlan_id+' x '+dot10said)
+			os.system('snmpset -v2c -c private '+switch_ip+' vtpVlanEditOperation.1 i 3')
+		else:
+			os.system('snmpset -v2c -c private '+switch_ip+' vtpVlanEditOperation.1 i 2')
+			os.system('snmpset -v2c -c private '+switch_ip+' vtpVlanEditRowStatus.1.'+vlan_id+' i 6')
+			os.system('snmpset -v2c -c private '+switch_ip+' vtpVlanEditOperation.1 i 3')
+def edit_ports_on_vlan():
+	pass
+	#vmVlan OID not found, TODO
