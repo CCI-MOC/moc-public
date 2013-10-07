bd9224fca7de30156003a8e97ad46ac65ae4f7bc
LinZhaoliang <zhaoliangliu@gmail.com>
2013-10-07 16:35:14 -0400

    fixed classes

--- python-mocutils/scripts/spl_er.py
+++ python-mocutils/scripts/spl_er.py
@@ -10,10 +10,13 @@ class Node(Base):
     mac_addr=Column(String)
     manage_ip=Column(String)
     available=Column(Boolean)
-
-    #many to one mapping to group
     group_name=Column(String,ForeignKey('groups.name'))
+    port_id=Column(Integer,ForeignKey('ports.port_id'))
+
+    #Many to one mapping to group
     group=relationship("Group",backref=backref('nodes',order_by=node_id))
+    #One to one mapping to port
+    port=relationship("Port",backref=backref('node',uselist=False))
 
     def __init__(self,node_id,mac_addr="mac",manage_ip="10.0.0.1",available=True):
         self.node_id=node_id 
@@ -21,26 +24,27 @@ class Node(Base):
         self.manage_ip=manage_ip
         self.available=available
     def __repr__(self):
-        return "<Node(%r %r %r %r %r)"%(self.node_id,self.mac_addr,self.manage_ip,self.available,self.group)
+        return "<Node(%r %r %r %r %r %r)"%(self.node_id,self.mac_addr,self.manage_ip,self.available,self.group,self.port)
 
 """
-one to one mapping between group and vm
-one to one mapping between group and vlan
-many to one mapping between node and group
+One to one mapping between group and vm
+One to one mapping between group and vlan
+Many to one mapping between node and group
 """
 
 class Group(Base):
     __tablename__='groups'
     name=Column(String,primary_key=True)
     vm_name=Column(String,ForeignKey('vms.name'))
-    vlan_id=Column(Integer,ForeignKey('vlans.vlan_id'))
+    network_id=Column(Integer,ForeignKey('networks.network_id'))
+    
     vm=relationship("VM",backref=backref('group',uselist=False))
-    vlan=relationship("Vlan",backref=backref('group',uselist=False))
+    network=relationship("Network",backref=backref('group',uselist=False))
     def __init__(self,name="group1"):
         self.name=name
 
     def __repr__(self):
-        return "<Group(%r)>"%(self.name)
+        return "<Group(%r %r %r)>"%(self.name,self.vm,self.network)
 
 class VM(Base):
     __tablename__='vms'
@@ -51,11 +55,11 @@ class VM(Base):
         self.name="vm1"
         self.available=available
     def __repr__(self):
-        return "<VM( %r %r)>"%(self.name,self.available)
+        return "<VM(%r %r)>"%(self.name,self.available)
 
 class Network(Base):
     __tablename__='networks'
-    network_id=(Integer,primary_key=True)
+    network_id=Column(Integer,primary_key=True)
     network_technology=Column(String)
 
     def __init__(self,network_id,network_technology="vlan"):
@@ -87,7 +91,7 @@ class Port(Base):
         self.port_no=port_no
         self.switch_id=switch_id
     def __repr__(self):
-        return "Port(%r %r)"%(self.port_id,self.port_no)
+        return "Port(%r %r %r)"%(self.port_id,self.switch_id,self.port_no)
 
 
 class Switch(Base):
@@ -102,7 +106,7 @@ class Switch(Base):
         return "Switch(%r %r)"%(self.switch_id,self.script)
 
 
-engine=create_engine('sqlite:///spl7.db',echo=True)
+engine=create_engine('sqlite:///spl16.db',echo=True)
 Base.metadata.create_all(engine)
 Session=sessionmaker(bind=engine)
 session=Session()
@@ -110,10 +114,31 @@ c1=Node(1,"mac1","10.0.0.1")
 c2=Node(2,"mac2","10.0.0.2")
 c3=Node(3,"mac3","10.0.0.3")
 c4=Node(4,"mac4","10.0.0.4")
-c5=Node(5,"mac4","10.0.0.5")
+c5=Node(5,"mac5","10.0.0.5")
+c6=Node(6,"mac6","10.0.0.6")
+p1=Port(201,1,1)
+p2=Port(202,1,2)
+p3=Port(203,1,3)
+p4=Port(204,2,1)
+p5=Port(205,2,2)
+p6=Port(206,2,3)
 g1=Group("group1")
 vm1=VM("vm1")
-vlan1=Vlan(109)
-g1.nodes=[c2,c4]
-session.add(g1)
-session.commit()
+network1=Network(101,"vlan")
+sw1=Switch(1,"cisco_snmp.py")
+sw2=Switch(2,"tp_link.py")
+
+c1.port=p1
+c2.port=p2
+c3.port=p3
+c4.port=p4
+c5.port=p5
+c6.port=p6
+
+g1.vm=vm1
+g1.network=network1
+g1.nodes=[c1,c2,c3]
+
+#Check all nodes in g1 connect to same switch.
+#run the script, print the nodes
+
