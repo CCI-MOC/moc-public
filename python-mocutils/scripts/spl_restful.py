a9cf17cefe007d91c951c34d6e4a08ee2f5d4dd7
LinZhaoliang <zhaoliangliu@gmail.com>
2013-10-22 13:41:57 -0400

    GET POSTPUT DELETE

--- python-mocutils/scripts/spl_restful.py
+++ python-mocutils/scripts/spl_restful.py
@@ -1,23 +1,103 @@
-from flask import Flask
-from flask.ext import restful
+
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
-api = restful.Api(app)
+
+'''
+
+GET
+/groups
+/groups/group_name
+
+404 
+
+POST
+curl -i -H "Content-Type: application/json" 
+ -X POST -d '{"group_name":"group3","vm_name":"vm3","network_id":103}'
+  http://localhost:5000/groups
+
+PUT
+curl -i -H "Content-Type: application/json" 
+ -X PUT -d '{"network_id":600}' 
+ http://localhost:5000/groups/group1
+
+DELETE
+ curl -i -X DELETE http://localhost:5000/groups/group1
+
+'''
+
+groups = [
+    {
+        'group_name': 'group1',
+        'vm_name': 'vm1',
+        'network_id': 101, 
+        'deployed': True
+    },
+    {
+        'group_name': 'group2',
+        'vm_name': 'vm2',
+        'network_id': 102, 
+        'deployed': True
+    }
+]
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/groups', methods = ['GET'])
+def get_groups():
+    return jsonify( { 'groups': groups } )
+
+@app.route('/groups/<group_name>', methods = ['GET'])
+def get_group(group_name):
+    group = filter(lambda g: g['group_name'] == group_name, groups)
+    if len(group) == 0:
+        abort(404)
+    return jsonify({'group':group[0]})
+
+@app.route('/groups', methods = ['POST'])
+def create_group():
+    if not request.json or not 'group_name' in request.json:
+        abort(400)
+    group = {
+        'group_name': request.json['group_name'],
+        'vm_name': request.json['vm_name'],
+        'network_id' : request.json['network_id'],
+        'deployed' : False
+        }
+    groups.append(group)
+    return jsonify({ 'group':group}), 201
 
 
-#The class is a resource
-class Group(restful.Resource):
-    def get(self):
-        return {
-	"group_name": "group1",
-	"vm_name": "vm1",
-	"network_id": 101,
-	"nodes": [ 1,2,3]
-}
+@app.route('/groups/<group_name>', methods = ['PUT'])
+def update_group(group_name):
+    group = filter(lambda g: g['group_name'] == group_name, groups)
+    if len(group) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'vm_name' in request.json and type(request.json['vm_name']) is not unicode:
+        abort(400)
+    if 'network_id' in request.json and type(request.json['network_id']) is not int:
+        abort(400)
+    if 'deployed' in request.json and type(request.json['deployed']) is not bool:
+        abort(400)
+    group[0]['vm_name'] = request.json.get('vm_name',group[0]['vm_name'])
+    group[0]['network_id'] = request.json.get('network_id',group[0]['network_id'])
+    group[0]['deployed'] = request.json.get('deployed',group[0]['deployed'])
+    return jsonify( { 'group':group[0]})
 
+@app.route('/groups/<group_name>', methods = ['DELETE'])
+def delete_group(group_name):
+    group = filter(lambda g: g['group_name'] == group_name, groups)
+    if len(group) == 0:
+        abort(404)
+    groups.remove(group[0])
+    return jsonify( { 'result':True })
 
-api.add_resource(Group, '/')
+    
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run(debug = True)
