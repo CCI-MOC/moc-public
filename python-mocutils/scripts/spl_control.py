08ff7364dea614451a0f459056c114409c372e74
Linzertorte <zhaoliangliu@gmail.com>
2013-10-27 12:38:55 -0400

    data file configurable

--- python-mocutils/scripts/spl_control.py
+++ python-mocutils/scripts/spl_control.py
@@ -1,16 +1,51 @@
 from spl_er import *
+import spl_config
 
 """
 string,string,integer
 """
 
-def init_db():
+def create_node_pool():
     keys=['node_id','mac_addr','manage_ip']
 
-    for line in open('nodes.txt'):
+    for line in open(spl_config.file_names["node"]):
         values=line.rstrip().split(' ')
         d=dict(zip(keys,values))
         session.add(Node(int(d['node_id']),d['mac_addr'],d['manage_ip']))
+    session.commit()
+
+def create_network_pool():
+    keys=['network_id','network_technology']
+    
+    for line in open(spl_config.file_names["network"]):
+        values=line.rstrip().split(" ")
+        d=dict(zip(keys,values))
+        session.add(Network(int(d["network_id"]),d["network_technology"]))
+    session.commit()
+
+def create_vm_pool():
+    keys=['vm_name']
+    for line in open(spl_config.file_names["vm"]):
+        values = line.rstrip().split(" ")
+        d = dict(zip(keys,values))
+        session.add(VM(d["vm_name"]))
+    session.commit()
+
+
+def create_switch_pool():
+    keys=["switch_id","script"]
+    for line in open(spl_config.file_names["switch"]):
+        values = line.rstrip().split(" ")
+        d = dict(zip(keys,values))
+        session.add(Switch(int(d["switch_id"]),d["script"]))
+    session.commit()
+
+def create_port_pool():
+    keys=["port_id","switch_id","port_no"]
+    for line in open(spl_config.file_names["port"]):
+        values = line.rstrip().split(" ")
+        d = dict(zip(keys,values))
+        session.add(Port(int(d["port_id"]),int(d["switch_id"]),int(d["port_no"])))
     session.commit()
 
 def query_db(classname):
@@ -28,80 +63,25 @@ def check_available(classname,cond):
 def get_entity_by_cond(classname,cond):
     return session.query(classname).filter(cond).first()
 
-def create_node_pool():
-    c1=Node(1,"mac1","10.0.0.1")
-    c2=Node(2,"mac2","10.0.0.2")
-    c3=Node(3,"mac3","10.0.0.3")
-    c4=Node(4,"mac4","10.0.0.4")
-    c5=Node(5,"mac5","10.0.0.5")
-    c6=Node(6,"mac6","10.0.0.6")
-    session.add(c1)
-    session.add(c2)
-    session.add(c3)
-    session.add(c4)
-    session.add(c5)
-    session.add(c6)
+def connect_node_to_port():
 
-def create_network_pool():
-    net1=Network(101,"vlan")
-    net2=Network(102,"vlan")
-    session.add(net1)
-    session.add(net2)
-
-def create_vm_pool():
-    vm1=VM("vm1")
-    vm2=VM("vm2")
-    session.add(vm1)
-    session.add(vm2)
-
-def create_switch_pool():
-    sw1=Switch(1,"cisco_snmp.py")
-    sw2=Switch(2,"tp_link.py")
-    session.add(sw1)
-    session.add(sw2)
-
-def create_port_pool():
-    p1=Port(201,1,1)
-    p2=Port(202,1,2)
-    p3=Port(203,1,3)
-    p4=Port(204,2,1)
-    p5=Port(205,2,2)
-    p6=Port(206,2,3)
-    session.add(p1)
-    session.add(p2)
-    session.add(p3)
-    session.add(p4)
-    session.add(p5)
-    session.add(p6)
-def connect_nodes_to_ports():
-    c1=get_entity_by_cond(Node,'node_id==1')
-    c2=get_entity_by_cond(Node,'node_id==2')
-    c3=get_entity_by_cond(Node,'node_id==3')
-    c4=get_entity_by_cond(Node,'node_id==4')
-    c5=get_entity_by_cond(Node,'node_id==5')
-    c6=get_entity_by_cond(Node,'node_id==6')
-
-    p1=get_entity_by_cond(Port,'port_id==201')
-    p2=get_entity_by_cond(Port,'port_id==202')
-    p3=get_entity_by_cond(Port,'port_id==203')
-    p4=get_entity_by_cond(Port,'port_id==204')
-    p5=get_entity_by_cond(Port,'port_id==205')
-    p6=get_entity_by_cond(Port,'port_id==206')
-    c1.port=p1
-    c2.port=p2
-    c3.port=p3
-    c4.port=p4
-    c5.port=p5
-    c6.port=p6
-
+    keys=["node_id","port_id"]
+    for line in open(spl_config.file_names["connect"]):
+        values = line.rstrip().split(" ")
+        d=dict(zip(keys,values))
+        node=get_entity_by_cond(Node,"node_id==%d"%int(d["node_id"]))
+        port=get_entity_by_cond(Port,"port_id==%d"%int(d["port_id"]))
+        node.port=port
+    session.commit()
+    
 def load_resources():
     create_node_pool()
     create_network_pool()
     create_vm_pool()
     create_switch_pool()
     create_port_pool()
-    connect_nodes_to_ports()
-    session.commit()
+    connect_node_to_port()
+    
 
 def add_node_to_group(node_id,group_name):
     node=get_entity_by_cond(Node,'node_id==%d'%node_id)
