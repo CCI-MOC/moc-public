544c073ceb0d4d48f235073ce7fb4accf9d874de
Zhaoliang Liu <liuzhaoliang1988@gmail.com>
2014-01-16 14:43:15 -0500

    packaged spl so we can install it

---
+++ python-mocutils/scripts/SPL/build/lib/spl_command_pattern.py
@@ -0,0 +1,46 @@
+import re
+
+help_text='''
+show all
+show <specific table>
+show free [specific table]
+show group <group_name>
+create vlan <vlan_id>
+destroy vlan <vlan_id>
+create group <group_name> vlan <vlan_id> head-node <vm_name>
+destroy group <group_name>
+add <node_ids> to <group_name>
+remove <node_ids> from <group_name>
+change <group_name> vlan to <vlan_id>
+change <group_name> head to <vm_name>
+deploy
+exit
+'''
+create_user = re.compile('^create user (\w+) (\w+)$')
+
+create_group = re.compile('^create group (\w+) vlan (\d+) head-node (\S+)')
+destroy_group = re.compile('^destroy group (\w+)$')
+
+add = re.compile('^add (\S+) to (\w+)$')
+# Remove a machine from a group
+remove = re.compile('^remove (\S+) from (\w+)$')
+
+# Create a vlan
+create_vlan = re.compile('^create vlan (\d+)')
+# Destroy a vlan
+destroy_vlan = re.compile('^destroy vlan (\d+)')
+
+# Show a specific group
+show_group = re.compile('^show group (\w+)')
+# Show the free elements of a table
+show_free_table = re.compile('^show free (\w+)')
+# Show a specific table
+show_table = re.compile('^show (\w+)')
+  
+
+# Change a group's vlan
+change_vlan = re.compile('^change (\w+) vlan to (\d+)')
+# Change a group's vm
+change_head = re.compile('^change (\w+) head to (\S+)')
+
+
---
+++ python-mocutils/scripts/SPL/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='spl',
+      version='1.0',
+      py_modules=['spl_control','spl_er','spl_command_pattern','spl_config'],
+      )
