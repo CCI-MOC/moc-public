f12146f9852a6f76f71276c506270147d1a108cb
Zhaoliang Liu <liuzhaoliang1988@gmail.com>
2014-01-16 14:55:52 -0500

    deps

--- python-mocutils/scripts/SPL/setup.py
+++ python-mocutils/scripts/SPL/setup.py
@@ -1,5 +1,7 @@
-from distutils.core import setup
+
+from setuptools import setup, find_packages
 setup(name='spl',
       version='1.0',
       py_modules=['spl_control','spl_er','spl_command_pattern','spl_config'],
+      install_requires=['flask','sqlalchemy','Flask-HTTPAuth'],
       )
