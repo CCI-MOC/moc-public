89fda74edcc054152219a3f194793b9bec355829
Ian Denhardt <ian@zenhack.net>
2014-01-21 14:17:20 -0500

    Some packaging related cleanups
    - Deleted the manifest. The auto-detection is closer to what we want
      already, and there's no reason we need to specify this manually.
    - Added scripts (restful, shell, init) to setup.py, so that they'll be
      packaged.
    - Added a url to our setup.py (this is required).
    
    We're still not supplying an author/author_email, which is required.
    We'll have to make a decision as to what to put there.

--- SPL/setup.py
+++ SPL/setup.py
@@ -2,6 +2,8 @@
 from setuptools import setup, find_packages
 setup(name='spl',
       version='1.0',
+      url='https://github.com/CCI-MOC/moc-public',
       packages=['spl'],
+      scripts=['spl_init.py', 'spl_restful.py', 'spl_shell.py'],
       install_requires=['flask','sqlalchemy','Flask-HTTPAuth'],
       )
