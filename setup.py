1858a0a0f3b168115ecd2faed1f955456db268b8
Ian Denhardt <ian@zenhack.net>
2014-01-24 17:20:15 -0500

    setup.py: autodetect packages with find_packages()
    I plan on splitting things out into multiple packages; this will make
    things easier.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(name='spl',
       version='1.0',
       url='https://github.com/CCI-MOC/moc-public',
-      packages=['spl'],
+      packages=find_packages(),
       scripts=['spl_init.py', 'spl_restful.py', 'spl_shell.py'],
       install_requires=[
           'flask',
