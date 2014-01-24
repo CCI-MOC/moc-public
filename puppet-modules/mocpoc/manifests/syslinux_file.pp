13df9bb5e5ad3a018b00e29a740aa191831ae44b
Ian Denhardt <ian@zenhack.net>
2014-01-24 16:36:45 -0500

    Moved SPL to the root directory, deleted the rest.
    Nothing else in the repository was actually relevant anymore; It's just
    the SPL we care about now.
    
    This also involved some rearranging/cleaning up our .gitignore. No need
    to have rules for things like vmlinuz anymore.

--- puppet-modules/mocpoc/manifests/syslinux_file.pp
+++
@@ -1,8 +0,0 @@
-# A defined type for the syslinux boot files which are copied from the
-# syslinux-common package.
-define mocpoc::syslinux_file ($filename = $title) {
-	file { "/var/lib/tftpboot/$filename":
-		source => "/usr/lib/syslinux/$filename",
-		require => Package['syslinux-common', 'tftpd-hpa'],
-	}
-}
