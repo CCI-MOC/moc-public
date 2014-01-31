9e4f2ae08e38c18a86a108ecc9f16cabc9351252
Zhaoliang Liu <liuzhaoliang1988@gmail.com>
2014-01-31 12:16:45 -0500

    initial data

--- spl_test.py
+++ spl_test.py
@@ -1,21 +1,26 @@
-from spl.control import *
+import spl.control
 
-#import os
-#import os.path
-#
-#db_filename = 'spl17.db'
-#
-#if os.path.exists(db_filename):
-#    os.remove(db_filename)
+spl.control.create_node(2)
+spl.control.create_node(3)
+spl.control.create_node(4)
 
-load_resources()
-query_db(Node)
-query_db(VM)
-query_db(Network)
-create_group("group1","vlan-head-108",101)
-query_db(Group)
-add_node_to_group(1,"group1")
-add_node_to_group(4,"group1")
-add_node_to_group(6,"group1")
-deploy_group("group1")
-query_db(Group)
+
+spl.control.create_nic(2,'mac2','pxe')
+spl.control.create_nic(3,'mac3','pxe')
+spl.control.create_nic(4,'mac4','pxe')
+
+spl.control.add_nic(2,2)
+spl.control.add_nic(3,3)
+spl.control.add_nic(4,4)
+
+spl.control.create_switch(1,'dell')
+
+spl.control.create_port(2,1,2)
+spl.control.create_port(3,1,3)
+spl.control.create_port(4,1,4)
+
+spl.control.connect_nic(2,2)
+spl.control.connect_nic(3,3)
+spl.control.connect_nic(4,4)
+
+
